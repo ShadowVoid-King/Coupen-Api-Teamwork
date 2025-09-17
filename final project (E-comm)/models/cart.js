d3aea6ab90d8992edc36a60b3bce932904ea3a70
Reem Mohamed <ream2004523farouk@gmail.com>
2025-09-17 12:35:12 +0300

    Update cart.js

--- final project (E-comm)/models/cart.js
+++ final project (E-comm)/models/cart.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 const CartSchema= new mongoose.Schema({
     productName:{
-        type:String,
+        type:[String],
         required:true
     },
     username:{
