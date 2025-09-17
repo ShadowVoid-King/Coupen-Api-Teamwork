5809f989f21238c9c0a1b6e59692b89467efdae3
Reem Mohamed <ream2004523farouk@gmail.com>
2025-09-17 14:26:20 +0300

    Update shoppingCart.js

--- final project (E-comm)/router/shoppingCart.js
+++ final project (E-comm)/router/shoppingCart.js
@@ -1,10 +1,9 @@
 const { AddToCart, DeleteFromCart } = require("../controllers/cart");
-const { CheckAuth } = require("../middelware/checkAuth");
 const checkAuth = require("../middleware/checkAuth");
 
 const router = require("express").Router();
 
-router.use(CheckAuth);
+router.use(checkAuth);
 router.post("/add", checkAuth, AddToCart);
 router.delete("/delete", checkAuth, DeleteFromCart);
 
