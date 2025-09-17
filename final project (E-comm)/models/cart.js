0c9019dea08b6de6cf9ea107812ef811734e6b76
Reem Mohamed <ream2004523farouk@gmail.com>
2025-09-17 12:05:42 +0300

    upload cart model

---
+++ final project (E-comm)/models/cart.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose')
+
+const CartSchema= new mongoose.Schema({
+    productName:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true
+    }
+    
+})
+const CartData=mongoose.model('cartdata',CartSchema)
+module.exports={CartData}
